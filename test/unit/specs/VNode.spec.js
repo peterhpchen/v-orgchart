ab7f4af75e4360822f67becb0d83c45ec2816361
peterhpchen <peterhsinpingchen@gmail.com>
2018-04-27 00:20:23 +0800

    add: add vue test util.

--- test/unit/specs/VNode.spec.js
+++ test/unit/specs/VNode.spec.js
@@ -1,45 +1,43 @@
-import Vue from 'vue';
+// import Vue from 'vue';
+import { shallow } from '@vue/test-utils';
 import VNode from '@/components/VNode';
 
 describe('VNode.vue', () => {
-  const data = {
-    name: 'parent',
-    id: 1,
-    children: [
-      {
-        name: 'child1',
-        id: 11,
-        children: [
-          {
-            name: 'child11',
-            id: 111,
-          },
-        ],
-      },
-      {
-        name: 'child2',
-        id: 12,
-      },
-      {
-        name: 'child3',
-        id: 13,
-      },
-    ],
-  };
+  let data;
+
+  beforeEach(() => {
+    data = {
+      name: 'A',
+      id: 1,
+      children: [
+        {
+          name: 'AA',
+          id: 11,
+          children: [
+            {
+              name: 'AAA',
+              id: 111,
+            },
+          ],
+        },
+        {
+          name: 'AB',
+          id: 12,
+        },
+        {
+          name: 'AC',
+          id: 13,
+        },
+      ],
+    };
+  });
+
   it('should match snapshot', () => {
-    const Constructor = Vue.extend(VNode);
-    const vm = new Constructor({
+    const wrapper = shallow(VNode, {
       propsData: {
         nodeData: data,
       },
-      // slots: {
-      //   default: {
-      //     render(h) {
-      //       return h('template');
-      //     },
-      //   },
-      // },
-    }).$mount();
-    expect(vm.$el.outerHTML).toMatchSnapshot();
+    });
+    expect(wrapper.html()).toMatchSnapshot();
   });
 });
