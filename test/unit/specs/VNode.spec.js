f11ac3f7cb6401f87da980c556d29ca8f0543874
peterhpchen <peterhsinpingchen@gmail.com>
2018-04-30 02:17:59 +0800

    refactor: make the test data more simple

--- test/unit/specs/VNode.spec.js
+++ test/unit/specs/VNode.spec.js
@@ -3,39 +3,14 @@ import { shallow } from '@vue/test-utils';
 import VNode from '@/components/VNode';
 
 describe('VNode.vue', () => {
-  let data;
-
-  beforeEach(() => {
-    data = {
-      name: 'A',
-      id: 1,
-      children: [
-        {
-          name: 'AA',
-          id: 11,
-          children: [
-            {
-              name: 'AAA',
-              id: 111,
-            },
-          ],
-        },
-        {
-          name: 'AB',
-          id: 12,
-        },
-        {
-          name: 'AC',
-          id: 13,
-        },
-      ],
-    };
-  });
-
   it('should match snapshot', () => {
+    const node = {
+      name: 'A',
+    };
+
     const wrapper = shallow(VNode, {
       propsData: {
-        nodeData: data,
+        nodeData: node,
       },
       scopedSlots: {
         default: '<div class="name" slot-scope="props">{{props.nodeData.name}}</div>',
