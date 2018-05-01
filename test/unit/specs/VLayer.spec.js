a7348b262dda7d43ebe05217dde46a2cf7cc45f1
peterhpchen <peterhsinpingchen@gmail.com>
2018-05-01 08:26:53 +0800

    feature: make nodes tr test case more accurate

--- test/unit/specs/VLayer.spec.js
+++ test/unit/specs/VLayer.spec.js
@@ -3,14 +3,33 @@ import VLayer from '@/components/VLayer';
 
 describe('VLayer.vue', () => {
   describe('given a data with children property', () => {
-    it('have nodes', () => {
+    it('have a nodes tr in the last row', () => {
       const node = {
         name: 'A',
         children: [
           {
             name: 'B',
-          }
-        ]
+          },
+        ],
+      };
+
+      const wrapper = shallow(VLayer, {
+        propsData: {
+          data: node,
+        },
+      });
+
+      expect(wrapper.contains('tr:last-child.nodes')).toBeTruthy();
+    });
+
+    it('have two lines tr', () => {
+      const node = {
+        name: 'A',
+        children: [
+          {
+            name: 'B',
+          },
+        ],
       };
 
       const wrapper = shallow(VLayer, {
@@ -24,7 +43,7 @@ describe('VLayer.vue', () => {
   });
 
   describe('given a data without children property', () => {
-    it('does not have any nodes', () => {
+    it('does not have any nodes tr', () => {
       const node = {
         name: 'A',
       };
