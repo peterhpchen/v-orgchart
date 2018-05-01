f9aadf94fb37d188cdc4fae3cb7a8dcce0c14dcc
peterhpchen <peterhsinpingchen@gmail.com>
2018-05-01 08:35:37 +0800

    add: fail test case(it have lines tr in the second and third row)

--- test/unit/specs/VLayer.spec.js
+++ test/unit/specs/VLayer.spec.js
@@ -3,6 +3,44 @@ import VLayer from '@/components/VLayer';
 
 describe('VLayer.vue', () => {
   describe('given a data with children property', () => {
+    it('have two lines tr in the second row', () => {
+      const node = {
+        name: 'A',
+        children: [
+          {
+            name: 'B',
+          },
+        ],
+      };
+
+      const wrapper = shallow(VLayer, {
+        propsData: {
+          data: node,
+        },
+      });
+
+      expect(wrapper.contains('tr:nth-child(2).lines')).toBeTruthy();
+    });
+
+    it('have two lines tr in the third row', () => {
+      const node = {
+        name: 'A',
+        children: [
+          {
+            name: 'B',
+          },
+        ],
+      };
+
+      const wrapper = shallow(VLayer, {
+        propsData: {
+          data: node,
+        },
+      });
+
+      expect(wrapper.contains('tr:nth-child(3).lines')).toBeTruthy();
+    });
+
     it('have a nodes tr in the last row', () => {
       const node = {
         name: 'A',
@@ -22,24 +60,6 @@ describe('VLayer.vue', () => {
       expect(wrapper.contains('tr:last-child.nodes')).toBeTruthy();
     });
 
-    it('have two lines tr', () => {
-      const node = {
-        name: 'A',
-        children: [
-          {
-            name: 'B',
-          },
-        ],
-      };
-
-      const wrapper = shallow(VLayer, {
-        propsData: {
-          data: node,
-        },
-      });
-
-      expect(wrapper.contains('.nodes')).toBeTruthy();
-    });
   });
 
   describe('given a data without children property', () => {
