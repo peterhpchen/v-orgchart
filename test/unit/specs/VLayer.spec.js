0620346a2870304b76bf2c66ceb05f8eb227bb80
peterhpchen <peterhsinpingchen@gmail.com>
2018-05-03 00:01:40 +0800

    refactor: use beforeEach initialize wrapper

--- test/unit/specs/VLayer.spec.js
+++ test/unit/specs/VLayer.spec.js
@@ -3,7 +3,9 @@ import VLayer from '@/components/VLayer';
 
 describe('VLayer.vue', () => {
   describe('given a data with single child', () => {
-    it('has lines tr in the second row', () => {
+    let wrapper;
+
+    beforeEach(() => {
       const node = {
         name: 'A',
         children: [
@@ -13,106 +15,33 @@ describe('VLayer.vue', () => {
         ],
       };
 
-      const wrapper = shallow(VLayer, {
+      wrapper = shallow(VLayer, {
         propsData: {
           data: node,
         },
       });
+    });
 
+    it('has lines tr in the second row', () => {
       expect(wrapper.contains('tr:nth-child(2).lines')).toBeTruthy();
     });
 
     it('has downLine in the second row', () => {
-      const node = {
-        name: 'A',
-        children: [
-          {
-            name: 'B',
-          },
-        ],
-      };
-
-      const wrapper = shallow(VLayer, {
-        propsData: {
-          data: node,
-        },
-      });
-
       expect(wrapper.contains('tr:nth-child(2) > td > div.downLine')).toBeTruthy();
     });
 
     it('has rightLine in the first td of the third tr', () => {
-      const node = {
-        name: 'A',
-        children: [
-          {
-            name: 'B',
-          },
-        ],
-      };
-
-      const wrapper = shallow(VLayer, {
-        propsData: {
-          data: node,
-        },
-      });
-
       expect(wrapper.contains('tr:nth-child(3) > td:first-child.rightLine')).toBeTruthy();
     });
     it('has leftLine in the last td of the third tr', () => {
-      const node = {
-        name: 'A',
-        children: [
-          {
-            name: 'B',
-          },
-        ],
-      };
-
-      const wrapper = shallow(VLayer, {
-        propsData: {
-          data: node,
-        },
-      });
-
       expect(wrapper.contains('tr:nth-child(3) > td:last-child.leftLine')).toBeTruthy();
     });
 
     it('has lines tr in the third row', () => {
-      const node = {
-        name: 'A',
-        children: [
-          {
-            name: 'B',
-          },
-        ],
-      };
-
-      const wrapper = shallow(VLayer, {
-        propsData: {
-          data: node,
-        },
-      });
-
       expect(wrapper.contains('tr:nth-child(3).lines')).toBeTruthy();
     });
 
     it('has a nodes tr in the last row', () => {
-      const node = {
-        name: 'A',
-        children: [
-          {
-            name: 'B',
-          },
-        ],
-      };
-
-      const wrapper = shallow(VLayer, {
-        propsData: {
-          data: node,
-        },
-      });
-
       expect(wrapper.contains('tr:last-child.nodes')).toBeTruthy();
     });
   });
