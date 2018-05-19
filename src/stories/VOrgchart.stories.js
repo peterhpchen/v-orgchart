8d6002b558c47d655665edd426f1be7329d4e53d
peterhpchen <peterhsinpingchen@gmail.com>
2018-05-19 22:20:18 +0800

    add: async data story

--- src/stories/VOrgchart.stories.js
+++ src/stories/VOrgchart.stories.js
@@ -2,41 +2,84 @@ import { storiesOf } from '@storybook/vue';
 
 import VOrgchart from '../components/VOrgchart';
 
-storiesOf('VOrgchart', module).add('with children', () => ({
-  components: { VOrgchart },
-  template: `
-  <div id="app">
-    <v-orgchart
-      :data="data"
-    />
-  </div>
-  `,
-  data() {
-    return {
-      data: {
-        name: 'parent',
-        id: 1,
-        children: [
-          {
-            name: 'child1',
-            id: 11,
-            children: [
-              {
-                name: 'child11',
-                id: 111,
-              },
-            ],
-          },
-          {
-            name: 'child2',
-            id: 12,
-          },
-          {
-            name: 'child3',
-            id: 13,
-          },
-        ],
-      },
-    };
-  },
-}));
+storiesOf('VOrgchart', module)
+  .add('with children', () => ({
+    components: { VOrgchart },
+    template: `
+      <div id="app">
+        <v-orgchart
+          :data="data"
+        />
+      </div>
+    `,
+    data() {
+      return {
+        data: {
+          name: 'parent',
+          id: 1,
+          children: [
+            {
+              name: 'child1',
+              id: 11,
+              children: [
+                {
+                  name: 'child11',
+                  id: 111,
+                },
+              ],
+            },
+            {
+              name: 'child2',
+              id: 12,
+            },
+            {
+              name: 'child3',
+              id: 13,
+            },
+          ],
+        },
+      };
+    },
+  }))
+  .add('async data', () => ({
+    components: { VOrgchart },
+    template: `
+      <div id="app">
+        <v-orgchart
+          :data.sync="data"
+        />
+      </div>
+    `,
+    data() {
+      return {
+        data: new Promise((resolve) => {
+          setTimeout(() => {
+            resolve({
+              name: 'parent',
+              id: 1,
+              children: [
+                {
+                  name: 'child1',
+                  id: 11,
+                  children: [
+                    {
+                      name: 'child11',
+                      id: 111,
+                    },
+                  ],
+                },
+                {
+                  name: 'child2',
+                  id: 12,
+                },
+                {
+                  name: 'child3',
+                  id: 13,
+                },
+              ],
+            });
+          }, 1000);
+        }),
+      };
+    },
+  }));
